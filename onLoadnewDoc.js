df0bae0f4b7800ca9195879a6cd096cdb4c6c0a6
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-19 16:00:56 +0200

    Update onLoadnewDoc.js

--- onLoadnewDoc.js
+++ onLoadnewDoc.js
@@ -4,7 +4,7 @@ function onLoadJS(fNameJS) {
 		s.id = 'jQ001_001_init';
 		s.type = 'text/javascript';
 		//s.src = 'https://dl.dropboxusercontent.com/u/31117393/h1/iPlg/Today16/' + fNameJS + '.js';
-		s.src = 'https://raw.githubusercontent.com/SyzraelGithub/Today16/master/' + fNameJS + '.js';
+		s.src = 'https://rawgit.com/SyzraelGithub/Today16/master/' + fNameJS + '.js';
 		document.head.appendChild(s)
 	} else {
 	}
