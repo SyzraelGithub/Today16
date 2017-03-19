bcefe96323350a9ac97e7245eb05d3f023fcab9d
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-19 15:58:33 +0200

    Update Today16.js

--- Today16.js
+++ Today16.js
@@ -1,5 +1,5 @@
 //urlPF = 'https://dl.dropboxusercontent.com/u/31117393/h1/iPlg/Today16/';
-urlPF = 'https://raw.githubusercontent.com/SyzraelGithub/Today16/master/';
+urlPF = 'https://rawgit.com/SyzraelGithub/Today16/master/';
 with (new XMLHttpRequest()) {open('GET',urlPF + 'autLsynF.js',false);send();eval(responseText)}
 eval(autLsynF(urlPF + 'autLsynJS.js'));
 this.__global__ = this;
