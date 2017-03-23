9f77f86af3f4dde899f236010bb5d016dd81c7eb
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-23 20:19:10 +0200

    Update onLoadInit.js

--- onLoadInit.js
+++ onLoadInit.js
@@ -62,7 +62,7 @@ createScript = function(src,id,parent,type) {
 }
 
 createScript(scrArr.shift());
-setTimeout(createScripts,5000);
+setTimeout(createScripts,5000); // Değişiklik
 
 isiOS = function() {
 	var iDevices = [
