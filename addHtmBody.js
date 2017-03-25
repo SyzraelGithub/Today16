5803f0f2e0fa7c0f5b45d005f86e7c40e7598a7e
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-25 16:35:13 +0200

    Update addHtmBody.js

--- addHtmBody.js
+++ addHtmBody.js
@@ -2,7 +2,11 @@ function addHtmBody(htmDoc) {
 	var inBodyDoc = autLsynHDoc('defDiv');
 	//00:00 burada değişebilir.
 	//htmDoc.body.innerHTML = inBodyDoc.body.innerHTML;
-	alert(dDt.newHourMinute);
+	if (dDt.dDate.toLocaleDateString() == new Date().toLocaleDateString()) {
+		alert(dDt.newHourMinute);
+	} else {
+		alert('11:11');
+	}
 	htmDoc.body.innerHTML = Array(9).join(inBodyDoc.body.innerHTML);
 	//Eski Today.html Satır: 649
 	return htmDoc;
