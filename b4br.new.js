dfad0986e28d7a1df5f94714d4f9e4a3e35b85a5
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-25 09:23:48 +0200

    Delete b4br.new.js

--- b4br.new.js
+++
@@ -1,7 +0,0 @@
-function b4br(el, bD, br){
-	var bD = bD || document.createElement('br');
-	br = br || 'BR';
-	if (el.previousSibling.nodeName != br || bD.tagName != br) {
-		el.parentElement.insertBefore(bD, el);
-	}
-}
