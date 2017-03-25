7f61a1413674752cf19f628d9e27499240708d4e
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-25 09:37:39 +0200

    Delete creDoc.new.js

--- creDoc.new.js
+++
@@ -1,9 +0,0 @@
-function creDoc(innerText,fldLCrr,isView) {
-	fldLCrr = fldLCrr || fldL;
-	isView = isView || true;
-	var newDoc = creEmpDoc(fldLCrr, innerText);
-	var htmDoc = creHtmDoc();
-	htmDoc = preHtmDoc(htmDoc);
-	newDoc.SetHtml(htmDoc.documentElement.outerHTML);
-	if (isView) {vieDoc(newDoc.GUID)}
-}
