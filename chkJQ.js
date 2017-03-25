a2897223847344ca0e48a71187862f946f71658d
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-25 09:43:27 +0200

    Update chkJQ.js

--- chkJQ.js
+++ chkJQ.js
@@ -1,1 +1,15 @@
-alert('chkJQ.js loaded');
+var dfnc = '';
+var dfn$ = '';
+
+if (typeof cO != 'undefined') {
+  dfnc = 'cO == undefined';
+} else {
+  dfnc = 'cO == defined';
+}
+if (typeof $ != 'undefined') {
+  dfn$ = '$ == undefined';
+} else {
+  dfn$ = '$ == defined';
+}
+
+console.log(dfnc + '|' + dfn$);
