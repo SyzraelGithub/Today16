bd2b4c2075b6a3dc01e580f48a49ff153d90a22d
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-25 17:50:57 +0200

    Update addHtmBody.js

--- addHtmBody.js
+++ addHtmBody.js
@@ -3,7 +3,7 @@ function addHtmBody(htmDoc) {
 	//00:00 burada değişebilir.
 	//htmDoc.body.innerHTML = inBodyDoc.body.innerHTML;
 	if (dDt.dDate.toLocaleDateString() == new Date().toLocaleDateString()) {
-		alert(dDt.newHourMinute);
+		alert(dDt.newHourMinute + ' ' + htmDoc.title);
 	} else {
 		alert('11:11');
 	}
