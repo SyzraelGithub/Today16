bf57bfaf9d123306b10f0908370d1b300dfabf6a
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-26 14:25:09 +0300

    Update addHtmBody.js

--- addHtmBody.js
+++ addHtmBody.js
@@ -1,10 +1,15 @@
 function addHtmBody(htmDoc) {
 	var inBodyDoc = autLsynHDoc('defDiv');
+	alert('if döngüsü start');
 	if (htmDoc.title == dDt.newFileName) {
+		alert('htmDoc.title == dDt.newFileName imiş.');
 		if (htmDoc.getElementsByTagName('span').length > 0) {
+			alert('span adedi 0 değil');
 			var spn = htmDoc.getElementsByTagName('span')[0];
 			if (spn.innerHTML.indexOf('00:00') != -1) {
+				alert('spn içerisinde 00:00 buldu');
 				spn.innerHTML = spn.innerHTML.replace('00:00',dDt.newHourMinute);
+				alert('spn değişti = ' + spn.innerHTML);
 			}
 		}
 	}
