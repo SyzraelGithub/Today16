ab9e460aca72627f8883361bae674d19672b7345
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-26 14:00:40 +0300

    Update creDoc.js

--- creDoc.js
+++ creDoc.js
@@ -3,9 +3,7 @@ function creDoc(innerText,fldLCur,isView) {
 	isView = isView || true;
 	var newDoc = creEmpDoc(fldLCur, innerText);
 	var htmDoc = creHtmDoc();
-	alert('htmDoc.title = ' + htmDoc.title);
 	htmDoc.title = innerText;
-	alert('htmDoc.title = ' + htmDoc.title);
 	htmDoc = preHtmDoc(htmDoc);
 	newDoc.SetHtml(htmDoc.documentElement.outerHTML);
 	if (isView || innerText == dDt.newFileName) {vieDoc(newDoc.GUID)}
