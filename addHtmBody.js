246297509bc9c5aef066a2ed0edca63a8805b3e1
SyzraelGithub <syzrael.github.17@gmail.com>
2017-03-26 10:37:16 +0300

    Update addHtmBody.js

--- addHtmBody.js
+++ addHtmBody.js
@@ -3,6 +3,7 @@ function addHtmBody(htmDoc) {
 	//00:00 burada değişebilir.
 	//htmDoc.body.innerHTML = inBodyDoc.body.innerHTML;
 	alert(typeof dDt);
+	alert(typeof dDt.newFileName);
 	if (htmDoc.title == dDt.newFileName) {
 		alert(dDt.newHourMinute + ' ' + htmDoc.title);
 	} else {
@@ -12,3 +13,4 @@ function addHtmBody(htmDoc) {
 	//Eski Today.html Satır: 649
 	return htmDoc;
 }
+.newFileName 
