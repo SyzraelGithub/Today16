5c5f58930c5532224fcbb77362d06df80813cf82
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-01 19:23:01 +0300

    Update autLsynF.js

--- autLsynF.js
+++ autLsynF.js
@@ -13,3 +13,12 @@ getPF = function() {
 		return 'https://rawgit.com/SyzraelGithub/Today16/master/'
 	}
 }
+
+var innMsg = 'autLsynF.js loaded [from autLsynF.js]';
+if (typeof __global__ != 'undefined') {
+	__global__.console.log(innMsg);
+	__global__.alert(innMsg);
+} else {
+	console.log(innMsg);
+	alert(innMsg);
+}
