55cadc14385c9907ba2def1b86707a25d5bf9fa5
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-01 19:23:33 +0300

    Update autLsynHDoc.js

--- autLsynHDoc.js
+++ autLsynHDoc.js
@@ -4,3 +4,12 @@ autLsynHDoc = function(urlLF) {
 	docF.documentElement.innerHTML = autLsynF(urlF);
 	return docF;
 }
+
+var innMsg = 'autLsynHDoc.js loaded [from autLsynHDoc.js]';
+if (typeof __global__ != 'undefined') {
+	__global__.console.log(innMsg);
+	__global__.alert(innMsg);
+} else {
+	console.log(innMsg);
+	alert(innMsg);
+}
