f91719fcaa5635ca22c2a1e1304c4f631dad63dc
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-01 20:56:00 +0300

    Update ProtoT_getWeek.js

--- ProtoT_getWeek.js
+++ ProtoT_getWeek.js
@@ -2,6 +2,7 @@ Date.prototype.getWeek = function() {
 	var onejan = new Date(this.getFullYear(),0,1);
 	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
 }
+/*
 var innMsg = 'ProtoT_getWeek.js loaded [from ProtoT_getWeek.js]';
 if (typeof __global__ != 'undefined') {
 	__global__.console.log(innMsg);
@@ -10,3 +11,4 @@ if (typeof __global__ != 'undefined') {
 	console.log(innMsg);
 	alert(innMsg);
 }
+*/
