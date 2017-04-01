daf8c5c935faada5325a0da944fd2f5cf8fed2db
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-01 16:19:49 +0300

    Update OnTodayBtnClicked16.js

--- OnTodayBtnClicked16.js
+++ OnTodayBtnClicked16.js
@@ -19,6 +19,6 @@ with(__global__.strObj){
 		objWindow.ShowSelectorWindow(helperFileName, arr0_2, arr1_3, 270, 230, '');
 	}
 }
-var innMsg = 'OnTodayBtnClicked16.js loaded';
+var innMsg = 'OnTodayBtnClicked16.js loaded [from OnTodayBtnClicked16.js]';
 __global__.console.log(innMsg);
 __global__.alert(innMsg);
