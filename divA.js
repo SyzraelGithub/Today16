fc41c75221c823a57711d193473919593b9e97f7
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-02 19:05:28 +0300

    Update divA.js

--- divA.js
+++ divA.js
@@ -5,13 +5,3 @@ divA.id = 'divA';
 divA.class = 'divAcls';
 
 document.body.appendChild(divA);
-/*
-var innMsg = 'divA.js loaded [from divA.js]';
-if (typeof __global__ != 'undefined') {
-	__global__.console.log(innMsg);
-	__global__.alert(innMsg);
-} else {
-	console.log(innMsg);
-	alert(innMsg);
-}
-*/
