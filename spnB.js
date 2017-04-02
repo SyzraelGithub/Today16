6361bb94ee14bb6a885b888ba4bd9fbdf0d8eec3
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-02 19:10:09 +0300

    Update spnB.js

--- spnB.js
+++ spnB.js
@@ -1,8 +1,4 @@
-//alert('spnB girildi');
-
 var spnB = document.createElement('span');
-
-//alert('spnB devam');
 
 spnB.id = 'spnB';
 spnB.class = 'spnBcls';
@@ -10,16 +6,8 @@ spnB.style.color = '#388e8e';
 spnB.style.textDecoration = 'underline';
 spnB.style.fontWeight = 'bold';
 
-//alert('spnB devam');
-
 prgB.appendChild(spnB);
-
-//alert('spnB devam');
 
 var dDt = new getdDt(new Date());
 
-//alert('spnB devam');
-
 spnB.innerText = dDt.myLocation;
-
-//alert('spnB son');
