3977f795348f1c7274d8ddbf5af97f141b0e5ec8
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-02 19:09:17 +0300

    Update prgB.js

--- prgB.js
+++ prgB.js
@@ -5,13 +5,3 @@ prgB.class = 'prgBcls';
 prgB.style.backgroundColor = '#dddddd';
 
 divA.appendChild(prgB);
-/*
-var innMsg = 'prgB.js loaded [from prgB.js]';
-if (typeof __global__ != 'undefined') {
-	__global__.console.log(innMsg);
-	__global__.alert(innMsg);
-} else {
-	console.log(innMsg);
-	alert(innMsg);
-}
-*/
