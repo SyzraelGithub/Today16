35ad67ccde469c9bfaa8c6fc041dc9c84bb03f35
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-02 18:58:15 +0300

    Update Today16.js

--- Today16.js
+++ Today16.js
@@ -1,21 +1,11 @@
 //urlPF = 'https://dl.dropboxusercontent.com/u/31117393/h1/iPlg/Today16/';
 urlPF = 'https://rawgit.com/SyzraelGithub/Today16/master/';
-//base url ile nerelerde karşılacaksın?
 with (new XMLHttpRequest()) {open('GET',urlPF + 'autLsynF.js',false);send();eval(responseText)}
-//alert(typeof autLsynF);
 eval(autLsynF(urlPF + 'autLsynJS.js'));
-//alert(typeof autLsynJS);
 this.__global__ = this;
 autLsynJS('GenDec');
-//alert(typeof objApp);
 autLsynJS('ProtoT_getWeek');
-//alert(typeof Date.prototype.getWeek);
 autLsynJS('twoDigit');
-//alert(typeof twoDigit);
 autLsynJS('GenDec_dDt');
 autLsynJS('varLoadF');
-//alert(autLsynF(urlPF + 'autLsynF.js'));
 autLsynJS('OnTodayBtnClicked16');
-//alert(autLsynF(urlPF + 'OnTodayBtnClicked16.js'));
-//alert(autLsynF(urlPF + 'Today16.html'));
-//alert('valla ha oldu!');
