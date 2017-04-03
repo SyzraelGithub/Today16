11f331316697d9b48e4c5072dcfae4a9c0f953ad
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-03 11:25:34 +0300

    Update varLoadF.js

--- varLoadF.js
+++ varLoadF.js
@@ -5,7 +5,7 @@ __global__.strObj = {
 	attachParent:'main',
 	btnName:'todayButton16',
 	btnFncName:'OnTodayBtnClicked16',
-	helperFileOnline:'https://rawgit.com/SyzraelGithub/Today16/master/Today16.html',
+	helperFileOnline: getPF() + 'Today16.html',
 	/*
 		helperFileOnline:getPF() veya urlPF + Today16.html
 		helperFilePart:'../../../../Dropbox/Public/h1/iPlg/Today16/Today16.html', => gerek yok
