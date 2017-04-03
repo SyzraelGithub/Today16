feeb4491407094fbd5ae34362e99bfe7ad3b937e
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-03 13:21:49 +0300

    Update addHtmOnLoad.js

--- addHtmOnLoad.js
+++ addHtmOnLoad.js
@@ -1,5 +1,5 @@
 function addHtmOnLoad(htmDoc) {
-	var newScr = conInLine(autLsynF(getPF() + 'onLoadnewDoc.js'));
+	var newScr = conInLine(autLsynF(urlPF + 'onLoadnewDoc.js'));
 	htmDoc.body.setAttribute('onload',newScr);
 	return htmDoc;
 }
