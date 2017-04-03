df98ae55e3bbf285e1e6eb088ff8e29a8f4e1e3e
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-03 13:16:32 +0300

    Update autLsynF.js

--- autLsynF.js
+++ autLsynF.js
@@ -2,7 +2,6 @@ if (typeof urlPF == 'undefined') {
 	if (document.getElementsByTagName('base').length > 0 {
 		urlPF = document.getElementsByTagName('base')[0].href;
 	}
-
 }
 
 autLsynF = function(urlF) {
