cf241cc3a2ecaaccc25429276a6db60729f183ff
SyzraelGithub <syzrael.github.17@gmail.com>
2017-04-03 13:16:22 +0300

    Update autLsynF.js

--- autLsynF.js
+++ autLsynF.js
@@ -1,19 +1,10 @@
-
 if (typeof urlPF == 'undefined') {
-	alert(typeof urlPF);
-	urlPF = document.getElementsByTagName('base')[0].href;
-	alert(typeof urlPF);
-}
+	if (document.getElementsByTagName('base').length > 0 {
+		urlPF = document.getElementsByTagName('base')[0].href;
+	}
 
+}
 
 autLsynF = function(urlF) {
 	with (new XMLHttpRequest()) {open('GET',urlF,false);send();return responseText}
-}
-
-getPF = function() {
-	if (typeof urlPF != 'undefined') {
-		return urlPF
-	} else if (document.getElementsByTagName('base').length > 0) {
-		return document.getElementsByTagName('base')[0].href
-	}
 }
