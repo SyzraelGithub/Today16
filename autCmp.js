30a107837d8294e07e081472489a65f4e3330941
SyzraelGithub <syzrael.github.17@gmail.com>
2017-07-29 18:01:57 +0300

    Update autCmp.js

--- autCmp.js
+++ autCmp.js
@@ -50,34 +50,39 @@ $(function() {
 		return split(term).pop();
 	}
 	
-	var blueSpans = $('div>p').filter(function(){return $(this).css('color')=='rgb(0, 0, 255)'}).children().filter(function(){return $(this).attr('role')!='status'});
+	if (document.body.contentEditable == 'true') {
+		
+		var blueSpans = $('div>p').filter(function(){return $(this).css('color')=='rgb(0, 0, 255)'}).children().filter(function(){return $(this).attr('role')!='status'});
+		
+		blueSpans.attr('contentEditable','true');
+		
+		blueSpans
+		// don't navigate away from the field on tab when selecting an item
+		.bind("keydown", function (event) {
+			if (event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
+				event.preventDefault();
+			}
+		}).autocomplete({
+			minLength: 0,
+			source: function (request, response) {
+				// delegate back to autocomplete, but extract the last term
+				response($.ui.autocomplete.filter(availableTags, extractLast(request.term)));
+			},
+			focus: function () {
+				// prevent value inserted on focus
+				return false;
+			},
+			select: function (event, ui) {
+				var value = $(this).html();
+				var terms = split(value);
+				terms.pop();
+				terms.push(ui.item.value);
+				$(this).html(value.substr(0, 6) + terms.join('.') + '.');
+				placeCaretAtEnd(this);
+				return false;
+			}
+		});
+
+	};
 	
-	blueSpans.attr('contentEditable','true');
-	
-	blueSpans
-	// don't navigate away from the field on tab when selecting an item
-	.bind("keydown", function (event) {
-		if (event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
-			event.preventDefault();
-		}
-	}).autocomplete({
-		minLength: 0,
-		source: function (request, response) {
-			// delegate back to autocomplete, but extract the last term
-			response($.ui.autocomplete.filter(availableTags, extractLast(request.term)));
-		},
-		focus: function () {
-			// prevent value inserted on focus
-			return false;
-		},
-		select: function (event, ui) {
-			var value = $(this).html();
-			var terms = split(value);
-			terms.pop();
-			terms.push(ui.item.value);
-			$(this).html(value.substr(0, 6) + terms.join('.') + '.');
-			placeCaretAtEnd(this);
-			return false;
-		}
-	});
 });
