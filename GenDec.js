30868219a42acc572dea0982b83237dd8a144ca1
SyzraelGithub <syzrael.github.17@gmail.com>
2017-07-29 14:30:10 +0300

    Update GenDec.js

--- GenDec.js
+++ GenDec.js
@@ -13,7 +13,29 @@ if (typeof thisTmp.objApp == 'undefined') {thisTmp.objApp = window.external;
 			}
 		} else {
 			thisTmp.objWizDoc = thisTmp.objApp.WizDocument;
-			thisTmp.objDatabase = thisTmp.objWizDoc.Database;
+			/*
+				1807291423
+				thisTmp.objWizDoc == undefined
+				if you open the note in an external browser unlike wiznote
+				for example in chrome or safari.
+				in this case, objDatabase returns an error
+				---
+				thisTmp.objDatabase = thisTmp.objWizDoc.Database;
+				---
+				So the line above, must be in an if clause
+			*/
+			if (typeof thisTmp.objWizDoc != 'undefined') {
+				thisTmp.objDatabase = thisTmp.objWizDoc.Database;
+			} else {
+				/*
+					Here should stand an inputbox, which asks to user,
+					whether body tag should turn contenteditable = true
+					---
+					It appears like you are opening the note in an external browser unlike wiznote.
+					Would you like to set body tag contenteditable = true?
+					YES / NO ?
+				*/
+			}
 		}
 	}
 }
