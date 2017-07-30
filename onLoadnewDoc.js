493487aaa74dd346e03a617e51ff97f97c837d90
SyzraelGithub <syzrael.github.17@gmail.com>
2017-07-30 16:39:21 +0300

    Update onLoadnewDoc.js

--- onLoadnewDoc.js
+++ onLoadnewDoc.js
@@ -1,7 +1,7 @@
 function onLoadJS(fNameJS) {
 	if (!document.getElementById(fNameJS)) {
 		var s = document.createElement('script');
-		s.id = 'jQ001_001_init';
+		s.id = 'jQ001_001_init';/*1707301638 fNameJS olması gerekmez miydi?*/
 		s.type = 'text/javascript';
 		s.src = '' + fNameJS + '.js';
 		document.head.appendChild(s)
