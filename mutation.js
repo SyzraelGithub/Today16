7ae670ead8c2080f0bf4a24ed188c217c5fc4d09
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-05 14:09:11 +0300

    Add files via upload

---
+++ mutation.js
@@ -0,0 +1,28 @@
+// select the target node
+var target = document.body;
+
+var mutationOut;
+ 
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+	  if (mutation.type == 'attributes') {
+		  if (mutation.attributeName == 'contenteditable') {
+			  var msg = 'contentEditable = ' + target.contentEditable;
+			  console.log(msg);
+			  alert(msg);
+		  }
+	  }
+    console.log(mutation.type);
+		mutationOut = mutation;
+  });    
+});
+ 
+// configuration of the observer:
+var config = { attributes: true } //, childList: true, characterData: true };
+ 
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
+ 
+// later, you can stop observing
+//observer.disconnect();
