699fedc5e695d43a01faee4b912898e255f923e0
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-05 14:09:53 +0300

    Update onLoadInit.js

--- onLoadInit.js
+++ onLoadInit.js
@@ -16,7 +16,8 @@ var scrArr = [
 	['autoTarNow',true],
 	['docRea',true],
 	['http://code.jquery.com/ui/1.10.3/jquery-ui.min.js',true],
-	['autCmp',true]
+	['autCmp',true],
+	['mutation',true]
 ];
 
 /*
