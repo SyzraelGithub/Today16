aadfdf9b9f3ef99882f4b967492a18b84e2f0db5
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-15 22:05:51 +0300

    Update autCmp.js

--- autCmp.js
+++ autCmp.js
@@ -43,7 +43,8 @@ function spanComplete(whichSpans) {
 			var terms = split(value);
 			terms.pop();
 			terms.push(ui.item.value);
-			$(this).html(value.substr(0, 6) + terms.join('.'));
+			//$(this).html(value.substr(0, 6) + terms.join('.'));
+			$(this).html(value.substr(0, 5) + ' ' + terms.join('.'));
 			console.log(terms);
 			console.log(terms.join('.'));
 			placeCaretAtEnd(this);
