8ac45afee524750037a0fb97b51cc5bfe72ca374
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-26 14:10:38 +0300

    Update spnE.js

--- spnE.js
+++ spnE.js
@@ -2,6 +2,11 @@ var spnE = document.createElement('span');
 
 spnE.id = 'spnE';
 spnE.class = 'spnEcls';
+
+spnE.style.color = '#388e8e';
+spnE.style.textDecoration = 'underline';
+spnE.style.fontWeight = 'bold';
+
 spnE.innerText = objDatabase.UserName;
 
 prgE.appendChild(spnE);
