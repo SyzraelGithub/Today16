a0cfd633c063ebc4cc1c590a24b14f30b912f1cc
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-28 19:24:22 +0300

    Update OnTodayBtnClicked16.js

--- OnTodayBtnClicked16.js
+++ OnTodayBtnClicked16.js
@@ -1,4 +1,7 @@
-with(__global__.strObj){
+var thisTmp = this;
+if (typeof __global__ != 'undefined') {thisTmp = __global__}
+
+with(thisTmp.strObj){
 	pluginPath = objApp.GetPluginPathByScriptFileName(jsFileName);
 	helperFileName = helperFileOnline;
 	languangeFileName = pluginPath + iniFileName;
