50303698a59a429b93a5d74e10d4850d69a168bd
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-28 19:55:08 +0300

    Update varLoadFPrc.js

--- varLoadFPrc.js
+++ varLoadFPrc.js
@@ -1,7 +1,7 @@
 var thisTmp = this;
 if (typeof __global__ != 'undefined') {thisTmp = __global__}
 
-with(__global__.strObj){
+with(thisTmp.strObj){
 	pluginPath = objApp.GetPluginPathByScriptFileName(jsFileName);
 	helperFileName = helperFileOnline;
 	languangeFileName = pluginPath + iniFileName;
