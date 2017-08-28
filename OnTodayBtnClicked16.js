8c235c63c91d53c5d9e0b7db91b3f24535b9ceaf
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-28 19:30:58 +0300

    Update OnTodayBtnClicked16.js

--- OnTodayBtnClicked16.js
+++ OnTodayBtnClicked16.js
@@ -2,10 +2,6 @@ var thisTmp = this;
 if (typeof __global__ != 'undefined') {thisTmp = __global__}
 
 with(thisTmp.strObj){
-	pluginPath = objApp.GetPluginPathByScriptFileName(jsFileName);
-	helperFileName = helperFileOnline;
-	languangeFileName = pluginPath + iniFileName;
-	todayStr = objApp.LoadStringFromFile(languangeFileName,stringName);
 	objWindow.AddToolButton(
 		attachParent,
 		btnName,
