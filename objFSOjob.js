8753b28e5e4e987b6be0ca7412ffb5b549128671
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-29 12:48:05 +0300

    Update objFSOjob.js

--- objFSOjob.js
+++ objFSOjob.js
@@ -3,7 +3,7 @@ if (typeof __global__ != 'undefined') {thisTmp = __global__}
 
 thisTmp.objFSO = objApp.CreateActiveXObject("Scripting.FileSystemObject");
 
-thisTmp.strObj.pluginUserPath = strObj.pluginPath + objDatabase.UserName + '\\';
+thisTmp.strObj.pluginUserPath = thisTmp.strObj.pluginPath + objDatabase.UserName + '\\';
 thisTmp.strObj.objFldUser = {};
 
 if (!thisTmp.objFSO.FolderExists(thisTmp.strObj.pluginUserPath)) {
@@ -13,6 +13,12 @@ if (!thisTmp.objFSO.FolderExists(thisTmp.strObj.pluginUserPath)) {
   console.log('Folder Var');
 }
 
+thisTmp.strObj.pluginStyPath = thisTmp.strObj.pluginUserPath + 'Styles\\';
+thisTmp.strObj.objFldSty = {};
+
+if (objDatabase.Styles.count != 0) {
+  thisTmp.strObj.objFldSty = objFSO.CreateFolder(strObj.pluginStyPath);
+}
 /*
   Eğer Style varsa, Style.js diye veya Style.json diye veya Style.??? diye bir dosya yarat.
   İçine Styles = {} Styles = []
