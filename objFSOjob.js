965a0ed48fbbb144c248ce99499a4b755ab264c8
SyzraelGithub <syzrael.github.17@gmail.com>
2017-08-30 10:47:57 +0300

    Update objFSOjob.js

--- objFSOjob.js
+++ objFSOjob.js
@@ -7,21 +7,21 @@ thisTmp.strObj.pluginUserPath = thisTmp.strObj.pluginPath + objDatabase.UserName
 thisTmp.strObj.objFldUser = {};
 
 if (!thisTmp.objFSO.FolderExists(thisTmp.strObj.pluginUserPath)) {
-  thisTmp.strObj.objFldUser = objFSO.CreateFolder(strObj.pluginUserPath);
-  console.log('Folder Yoktu Yaratıldı');
+	thisTmp.strObj.objFldUser = objFSO.CreateFolder(strObj.pluginUserPath);
+	console.log('Folder Yoktu Yaratıldı');
 } else {
-  console.log('Folder Var');
+	console.log('Folder Var');
 }
 
 thisTmp.strObj.pluginStyPath = thisTmp.strObj.pluginUserPath + 'Styles\\';
 thisTmp.strObj.objFldSty = {};
 
 if (objDatabase.Styles.count != 0) {
-  thisTmp.strObj.objFldSty = objFSO.CreateFolder(strObj.pluginStyPath);
+	thisTmp.strObj.objFldSty = objFSO.CreateFolder(strObj.pluginStyPath);
 }
 /*
-  Eğer Style varsa, Style.js diye veya Style.json diye veya Style.??? diye bir dosya yarat.
-  İçine Styles = {} Styles = []
+	Eğer Style varsa, Style.js diye veya Style.json diye veya Style.??? diye bir dosya yarat.
+	İçine Styles = {} Styles = []
 */
 
 var objStys = objDatabase.Styles;
