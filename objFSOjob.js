11a08f37e894ba5d8c54029d209f2b34cbab76d2
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-01 02:25:04 +0300

    Update objFSOjob.js

--- objFSOjob.js
+++ objFSOjob.js
@@ -3,5 +3,5 @@ if (typeof __global__ != 'undefined') {thisTmp = __global__}
 
 thisTmp.objFSO = objApp.CreateActiveXObject("Scripting.FileSystemObject");
 
-thisTmp.strObj.swpFldPath = thisTmp.strObj.pluginPath + 'Swap\\';
+thisTmp.strObj.swpFldPath = thisTmp.strObj.pluginPath + 'swp\\';
 thisTmp.strObj.swpFld = objFSO.CreateFolder(thisTmp.strObj.swpFldPath);
