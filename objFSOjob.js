8dab5df5af9ba8654423612789bbf48e8a0f5a94
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-01 03:50:14 +0300

    Update objFSOjob.js

--- objFSOjob.js
+++ objFSOjob.js
@@ -5,10 +5,10 @@ thisTmp.objFSO = objApp.CreateActiveXObject("Scripting.FileSystemObject");
 
 thisTmp.strObj.swpFldPth = thisTmp.strObj.pluginPath + 'swp\\';
 thisTmp.strObj.swpFld = objFSO.CreateFolder(thisTmp.strObj.swpFldPth);
-thisTmp.strObj.swpFilePth = thisTmp.strObj.swpFldPth + 'swpFile.json';
-thisTmp.strObj.swpFile = {};
+thisTmp.strObj.swpFilPth = thisTmp.strObj.swpFldPth + 'swpFil.json';
+thisTmp.strObj.swpFil = {};
 
-if (objFSO.FileExists(strObj.swpFilePath)) {
+if (objFSO.FileExists(strObj.swpFilPth)) {
   //Open
 } else {
   //Create
