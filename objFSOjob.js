543364115c72c957ffaa6126239505ec208c7371
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-02 16:24:24 +0300

    Update objFSOjob.js

--- objFSOjob.js
+++ objFSOjob.js
@@ -90,7 +90,7 @@ for (var userName in swapFileJSON["Users"]) {
 }
 
 slcE.onchange = function () {
-	alert('ghh');
+	alert('SEÇİM YAPMADAN DA ÇALIŞIYOR');
 	if (Object.keys(swapFileJSON["Users"][slcE.value]).length > 0) {
 		if (typeof swapFileJSON["Users"][slcE.value]['Styles'] != 'undefined') {
 			var swapStyles = swapFileJSON["Users"][slcE.value]['Styles'];
