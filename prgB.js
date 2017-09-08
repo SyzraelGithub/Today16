de0faa3d4ba597780dd77c663ddfce4feb9559a9
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-08 14:39:54 +0300

    Update prgB.js

--- prgB.js
+++ prgB.js
@@ -3,6 +3,5 @@ var prgB = document.createElement('p');
 prgB.id = 'prgB';
 prgB.class = 'prgBcls';
 prgB.style.backgroundColor = '#dddddd';
-prgB.style.margin = '1px 1px 1px 1px';
 
 divA.appendChild(prgB);
