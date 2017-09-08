66034744013e8737f9b153bccd81d58266413cf0
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-08 14:26:54 +0300

    Update prgE.js

--- prgE.js
+++ prgE.js
@@ -3,5 +3,6 @@ var prgE = document.createElement('p');
 prgE.id = 'prgE';
 prgE.class = 'prgEcls';
 prgE.style.backgroundColor = '#dddddd';
+prgE.style.margin = '1px 1px 1px 1px';
 
 divA.appendChild(prgE);
