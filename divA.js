b78cb8d1ea2e623b56ed880010e272487e2d5ff9
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-08 14:30:37 +0300

    Update divA.js

--- divA.js
+++ divA.js
@@ -4,5 +4,6 @@ document.body.style.margin = '1px 1px 1px 1px';
 var divA = document.createElement('div');
 divA.id = 'divA';
 divA.class = 'divAcls';
+divA.style.margin = '1px 1px 1px 1px';
 
 document.body.appendChild(divA);
