2f600ca39e71d95570d4918a1c0e56891cbe11c3
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-08 14:39:32 +0300

    Update divA.js

--- divA.js
+++ divA.js
@@ -1,9 +1,7 @@
 document.body.style.fontSize = '9pt';
-document.body.style.margin = '1px 1px 1px 1px';
 
 var divA = document.createElement('div');
 divA.id = 'divA';
 divA.class = 'divAcls';
-divA.style.margin = '1px 1px 1px 1px';
 
 document.body.appendChild(divA);
