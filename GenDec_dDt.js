f439a0e2e8ba0fca5d1262323b31013cb946323e
SyzraelGithub <syzrael.github.17@gmail.com>
2017-09-28 14:22:40 +0300

    Update GenDec_dDt.js

--- GenDec_dDt.js
+++ GenDec_dDt.js
@@ -8,7 +8,20 @@ getdDt = function(varDate) {
 		this.dWay = twoDigit(dDate.getDay());
 		this.dHour = twoDigit(dDate.getHours());
 		this.dMinute = twoDigit(dDate.getMinutes());
-		if (dWay == 0 || dWay == 6) {
+		/*
+			1709281419
+			Code 'un cumartesi günleri hata vermesinin sebebinin çalıştığı platformdaki
+			Haftanın ilk gününün değişiklik göstermesinden kaynaklandığını düşünüyordum.
+			Aslında halen de bir yandan düşünüyorum. 
+			Ancak, az önce debug ederken birşey daha farkettim.
+			if (dWay == 0 || dWay == 6) { //Buraya dWay String olarak geliyor.
+			dWay = "06" olarak geldiği için bence code şu şekilde olmalı. 
+			if (dWay == "00" || dWay == "06") {
+			Bu problemi çözer diye düşünüyorum. Deneyelim.
+			---
+		*/
+		if (dWay == "00" || dWay == "06") {
+		/*if (dWay == 0 || dWay == 6) {*/
 			dWeek-=1;
 			dWeek = twoDigit(dWeek);
 		};
