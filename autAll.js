228864a25f3d52005bf817bf0b7fccfcf6bb0ffa
SyzraelGithub <syzrael.github.17@gmail.com>
2017-10-26 10:11:31 +0300

    Update autAll.js

--- autAll.js
+++ autAll.js
@@ -24,15 +24,7 @@ function autNow() {
 function autAd2() {
 	if (!isOddTar(Tar)) {
 		var PreSib = getSib(Tar.previousElementSibling);
-		if (
-				(TarSib.innerText.length < 6)
-			&&
-				PreSib.innerText.substring(7,10) == '...'
-			&&
-				TarSib.innerText.substring(0,4) == 'a...'
-			&&
-				document.body.contentEditable = 'true'
-		) {
+		if (TarSib.innerText.length < 6 && PreSib.innerText.substring(7,10) == '...') {
 			PreSib.innerText = new getdDt(new Date()).newHourMinute + ' Ad2';
 			var sel = document.getSelection();
 			sel.collapse(TarSib,1);
