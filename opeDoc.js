4b44ec4e0b6320d2b0abcc945005f67c782fd0dd
SyzraelGithub <syzrael.github.17@gmail.com>
2018-05-05 17:42:25 +0300

    Add files via upload

---
+++ opeDoc.js
@@ -0,0 +1,14 @@
+function opeDoc(guid) {
+	
+	/*
+		1805051727
+		Gerekli müdaheleyi yaptıktan sonra görevi pointer 'ı tekrar vieDoc 'a bırakıyorum. Eskiden olduğu gibi
+		---
+		Öncelikle Today16.html dosyasına opeDoc tag 'ını ekleyelim. Ama nereye?
+			<script id="creDoc" src="creDoc.js"></script>
+			<script id="opeDoc" src="opeDoc.js"></script>
+			<script id="vieDoc" src="vieDoc.js"></script>
+			Bu araya ekledim. Ancak, yakında rprDoc da eklenmesi gerekiyor. Repair işlemi için.
+	*/
+	vieDoc(guid);
+}
