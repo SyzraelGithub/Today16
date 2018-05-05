a89390f7c4f9dde4f95e461c54b5b581e6b11501
SyzraelGithub <syzrael.github.17@gmail.com>
2018-05-05 20:13:37 +0300

    Update opeDoc.js

--- opeDoc.js
+++ opeDoc.js
@@ -1,6 +1,32 @@
 function opeDoc(guid) {
 	console.log('opeDoc içine guid = "' + guid + '" için girildi');
 	/*
+		1805052002
+		Şimdi MSI 'ın konsolundan aldığım kodları ters sırayla yazayım
+		---
+		newScr
+		"function onLoadJS(fNameJS) {if (!document.getElementById(fNameJS)) {var s = document.createElement('script');s.id = fNameJS; /*'jQ001_001_init'; 1707301638 fNameJS olması gerekmez miydi? Değiştirdim.*^/s.type = 'text/javascript';s.src = 'https://rawgit.com/SyzraelGithub/Today16/master/' + fNameJS + '.js';document.head.appendChild(s)}};onLoadJS('onLoadInit');"
+		---
+		var newScr = conInLine(autLsynF(urlPF + 'onLoadnewDoc.js'));
+		---
+		tmpImp.body.getAttribute('onload')
+		"function onLoadJS(fNameJS) {if (!document.getElementById(fNameJS)) {var s = document.createElement('script');s.id = fNameJS; /*'jQ001_001_init'; 1707301638 fNameJS olması gerekmez miydi? Değiştirdim.*^/s.type = 'text/javascript';s.src = 'https://rawgit.com/SyzraelGithub/Today16/master/' + fNameJS + '.js';document.head.appendChild(s)}};onLoadJS('onLoadInit');"
+		---
+		tmpImp.body
+		---
+		tmpImp
+		---
+		tmpImp.documentElement.innerHTML = objWizDoc.GetHtml()
+		---
+		var tmpImp = document.implementation.createHTMLDocument('');
+		---
+		document.implementation.createHTMLDocument('');
+		---
+		objWizDoc.GetHtml()
+		---
+		objWizDoc
+	*/
+	/*
 		1805051727
 		Gerekli müdaheleyi yaptıktan sonra görevi pointer 'ı tekrar vieDoc 'a bırakıyorum. Eskiden olduğu gibi
 		---
