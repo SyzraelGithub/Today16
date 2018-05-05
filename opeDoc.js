d7862e9abcce745f095388aab71e236d82395b20
SyzraelGithub <syzrael.github.17@gmail.com>
2018-05-05 17:41:41 +0300

    Delete opeDoc.js

--- opeDoc.js
+++
@@ -1,9 +0,0 @@
-
-/*
-  1805051727
-  Gerekli müdaheleyi yaptıktan sonra görevi pointer 'ı tekrar vieDoc 'a bırakıyorum. Eskiden olduğu gibi
-  ---
-  Öncelikle Today16.html dosyasına opeDoc tag 'ını ekleyelim. Ama nereye?
-*/
-
-vieDoc(guid);
