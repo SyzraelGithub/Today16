424ad984b6c4dabc00bfcbb895ac790c0e0f3caa
SyzraelGithub <syzrael.github.17@gmail.com>
2018-05-22 08:42:40 +0300

    Update addHtmBody.js

--- addHtmBody.js
+++ addHtmBody.js
@@ -8,7 +8,7 @@ function addHtmBody(htmDoc) {
 			}
 		}
 	}
-	htmDoc.body.innerHTML = Array(19).join(inBodyDoc.body.innerHTML);
+	htmDoc.body.innerHTML = Array(6).join(inBodyDoc.body.innerHTML);
 	//Eski Today.html Satır: 649
 	return htmDoc;
 }
