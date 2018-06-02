df583741b834c6bc0fd846f35862508629257364
SyzraelGithub <syzrael.github.17@gmail.com>
2018-06-02 17:35:53 +0300

    Update autAll.js

--- autAll.js
+++ autAll.js
@@ -53,7 +53,7 @@ function lstTar(lTar, lNum) {
 	if (
 		$('div:last-of-type').toArray().includes(Tar)
 		||
-		$('p:last-of-type').toArray().includes(Tar)
+		$('p:last').toArray().includes(Tar)
 	) {
 		autLasyHDoc(cO.e0,cbFncCur,3);
 	}
