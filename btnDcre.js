b19b7104d24f01a0ec6ed2f3e6d9969341f9f753
SyzraelGithub <syzrael.github.17@gmail.com>
2018-07-18 09:19:36 +0300

    change

--- btnDcre.js
+++ btnDcre.js
@@ -41,3 +41,10 @@ function btnDcre(idNum,innerText,isBold,guid) {
 	prgB.appendChild(btnD);
 	b4br(btnD);
 }
+
+/*
+	1807180918
+	Yeni not oluşturulduğunda, program baksa ve dese ki, sen son kullandığın hesapta değilsin?
+	Mesela syzrael.wiznote.18may hesabında değilsin 17dec desin 
+	Yine de yeni not eklemek istediğine emin misin?
+*/
