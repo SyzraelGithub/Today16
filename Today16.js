8b1b9560c4b90329c4bb803188f3980686517030
SyzraelGithub <syzrael.github.17@gmail.com>
2018-07-27 18:31:08 +0300

    ok

--- Today16.js
+++ Today16.js
@@ -1,6 +1,5 @@
 //urlPF = 'https://dl.dropboxusercontent.com/u/31117393/h1/iPlg/Today16/';
 urlPF = 'https://rawgit.com/SyzraelGithub/Today16/master/';
-alert('OK ilk satır');
 with (new XMLHttpRequest()) {open('GET',urlPF + 'autLsynF.js',false);send();eval(responseText)}
 eval(autLsynF(urlPF + 'autLsynJS.js'));
 this.__global__ = this;
