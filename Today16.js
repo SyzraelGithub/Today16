beea74cceede73d0731b7bd563a3d03f50141b8e
SyzraelGithub <syzrael.github.17@gmail.com>
2018-07-27 18:29:11 +0300

    ok

--- Today16.js
+++ Today16.js
@@ -1,31 +1,27 @@
 //urlPF = 'https://dl.dropboxusercontent.com/u/31117393/h1/iPlg/Today16/';
 urlPF = 'https://rawgit.com/SyzraelGithub/Today16/master/';
-alert('OK ilk satır' + urlPF);
+alert('OK ilk satır');
 with (new XMLHttpRequest()) {open('GET',urlPF + 'autLsynF.js',false);send();eval(responseText)}
-alert('OK 2inci satır');
 eval(autLsynF(urlPF + 'autLsynJS.js'));
-alert('OK 3uncu satır');
 this.__global__ = this;
-alert('OK 4uncu satır');
+//Multiload özelliği olsa, Today.js 'i GitHub klasöründe almak gerekmezdi. Offline version da örnek var.
+/*
+  1805140856
+  Sanırım benim burada anlatmaya çalıştığım, eğer multi load olsaydı,
+  autLsynJS sadece multiloadı yükleyecekti. Böylece, aşağıdaki js ler multiload da olacaktı
+  Değişme ihtimali azalacaktı dosyanın.
+  Değişirse multiload ın içeriği değişecekti. 
+  Bundan dolayı da bu dosya statik olabilirdi demek istemişim.
+*/
 autLsynJS('GenDec');
-alert('OK 5inci satır');
 autLsynJS('ProtoT_getWeek');
-alert('OK 6ıncı satır');
 autLsynJS('twoDigit');
-alert('OK 7inci satır');
 autLsynJS('GenDec_dDt');
-alert('OK 8inci satır');
 autLsynJS('varLoadF');
-alert('OK 9uncu satır');
-autLsynJS('varLoadFPrc');
-alert('OK 10uncu satır');
+autLsynJS('varLoadFPrc'); //Eğer GitHub klasöründen alınmıyorsa bu gözükmeyecek.
 autLsynJS('OnTodayBtnClicked16');
-alert('OK 11inci satır');
 
-
-
-
-
+//Sync Control
 
 
 
