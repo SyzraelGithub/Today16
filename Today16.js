2b7448107bf94976ef30cb40f07f78648275db3e
SyzraelGithub <syzrael.github.17@gmail.com>
2018-07-27 18:22:13 +0300

    ok

--- Today16.js
+++ Today16.js
@@ -3,3 +3,5 @@ urlPF = 'https://rawgit.com/SyzraelGithub/Today16/master/';
 alert('OK ilk satır' + urlPF);
 with (new XMLHttpRequest()) {open('GET',urlPF + 'autLsynF.js',false);send();eval(responseText)}
 alert('OK 2inci satır');
+eval(autLsynF(urlPF + 'autLsynJS.js'));
+alert('OK 3uncu satır');
