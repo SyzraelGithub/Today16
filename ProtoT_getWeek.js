a4c825f2f736a9b220e226d6c86fe849ed7fe061
SyzraelGithub <syzrael.github.17@gmail.com>
2021-10-07 14:17:39 +0300

    Update ProtoT_getWeek.js

--- ProtoT_getWeek.js
+++ ProtoT_getWeek.js
@@ -1,13 +1,53 @@
-Date.prototype.getWeekOld = function() {
+Date.prototype.getWeekOldOld = function() {
 	var onejan = new Date(this.getFullYear(),0,1);
 	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
 }
 
-Date.prototype.getWeek = function() {
+Date.prototype.getWeekOld = function() {
 	//https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
 	var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
 	var dayNum = d.getUTCDay() || 7;
 	d.setUTCDate(d.getUTCDate() + 4 - dayNum);
 	var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
 	return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
+}
+
+/* For a given date, get the ISO week number
+ *
+ * Based on information at:
+ *
+ *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ */
+
+Date.prototype.getWeek_Cmmntd = function() {
+    // Copy date so don't modify original
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return [d.getUTCFullYear(), weekNo];
+}
+
+Date.prototype.getWeek = function() {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    //return [d.getUTCFullYear(), weekNo];
+    return weekNo;
 }
