3e2b760626474319aaf3f1426be3587d1e00f469
SyzraelGithub <syzrael.github.17@gmail.com>
2021-10-07 18:33:38 +0300

    Update ProtoT_getWeek.js

--- ProtoT_getWeek.js
+++ ProtoT_getWeek.js
@@ -1,26 +1,13 @@
 Date.prototype.getWeekOld = function() {
-    var onejan = new Date(this.getFullYear(),0,1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+	var onejan = new Date(this.getFullYear(),0,1);
+	return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
 }
 
 Date.prototype.getWeek = function() {
-    //https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
-    /*
-        //2110071647
-        //Yukarıdaki linke uzun bir aradan sonra tekrar girince
-        //kodda değişiklik olduğunu farkettim
-        
-        var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
-        var dayNum = d.getUTCDay() || 7;
-        d.setUTCDate(d.getUTCDate() + 4 - dayNum);
-        var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-        return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
-    */
-    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
-    var dayNum = d.getUTCDay() || 7;
-    d.setUTCDate(d.getUTCDate() + 4 - (daynum);
-    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-    var weekNumber = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-    console.log (d + " için daynum = " + daynum)
-    return weekNumber;
+	//https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+	var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+	var dayNum = d.getUTCDay() || 7;
+	d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+	var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+	return Math.ceil((((d - yearStart) / 86400000) + 1)/7);
 }
